def3ebb8c295b40da726f649995685697f364c2c
Caique Lins <caique.c.lins@gmail.com>
2022-02-21 20:54:59 -0300

    Criando botões de tipo

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,6 +1,8 @@
 import Modal from "react-modal";
+import incomeImg from '../../assets/income.svg'
+import outcomeImg from '../../assets/outcome.svg'
 import closeImg from '../../assets/close.svg'
-import { Container } from "./styles";
+import { Container, TransactionTypeContainer } from "./styles";
 
 interface NewTransactionModalProps {
   isOpen: boolean;
@@ -17,14 +19,27 @@ export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionMo
       className="react-modal-content"
     >
       <button type="button" onClick={onRequestClose} className="react-modal-close">
-        <img src={closeImg} alt="Fechar Modal"/>
+        <img src={closeImg} alt="Fechar Modal" />
       </button>
       <Container>
         <h2>Cadastrar Transação</h2>
 
-        <input placeholder="Título"/>
-        <input placeholder="Valor" type="number"/>
-        <input placeholder="Categoria"/>
+        <input placeholder="Título" />
+        <input placeholder="Valor" type="number" />
+
+        <TransactionTypeContainer>
+          <button type="button">
+            <img src={incomeImg} alt="Entrada" />
+            <span>Entrada</span>
+          </button>
+
+          <button type="button">
+            <img src={outcomeImg} alt="Saída" />
+            <span>Saída</span>
+          </button>
+        </TransactionTypeContainer>
+
+        <input placeholder="Categoria" />
         <button type="submit">
           Cadastrar
         </button>
