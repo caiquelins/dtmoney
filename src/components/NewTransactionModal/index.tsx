2da06d2a14a23c09c290d552275d61b725a4d0bd
Caique Lins <caique.c.lins@gmail.com>
2022-02-21 21:47:27 -0300

    Inserindo transação na API

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -5,6 +5,7 @@ import closeImg from '../../assets/close.svg'
 
 import { Container, TransactionTypeContainer, RadioBox } from "./styles";
 import { FormEvent, useState } from "react";
+import { api } from "../../services/api";
 
 interface NewTransactionModalProps {
   isOpen: boolean;
@@ -19,8 +20,14 @@ export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionMo
 
   function handleCreateNewTransaction(event: FormEvent) {
     event.preventDefault();
-    console.log(title, value, category, type);
+    const data = {
+      title,
+      value,
+      category,
+      type
+    };
 
+    api.post('/transactions', data);
   }
 
   return (
